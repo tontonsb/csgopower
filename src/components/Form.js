0cd0baecc0652fae25aa02df8f1123688140977d
John Hartmann <jhartmann26@gmail.com>
2019-10-25 17:55:22 -0500

    added dragula
    todo: add dragula.on() to change list's rankings

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Dragula from "react-dragula";
 
 class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isFirstAttempt: true,
-            rankings: []
+            teams: [
+                { name: "Astralis", rank: 1 },
+                { name: "Liquid", rank: 2 },
+                { name: "EG", rank: 3 }
+            ]
         };
     }
     completeRankings() {
@@ -17,12 +22,26 @@ class Form extends React.Component {
         }
     }
     render() {
+        const listTeams = this.state.teams.map(team => (
+            <div className="formElem" key={team.name}>
+                {team.name}
+            </div>
+        ));
         return (
             <div>
+                <div className="teamForm" ref={this.dragulaDecorator}>
+                    {listTeams}
+                </div>
                 <button>submit</button>
             </div>
         );
     }
+    dragulaDecorator = componentBackingInstance => {
+        if (componentBackingInstance) {
+            let options = {};
+            Dragula([componentBackingInstance], options);
+        }
+    };
 }
 
 export default Form;
